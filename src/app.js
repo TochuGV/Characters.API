7d0bef497252e580344004eee36b19e7ed3bf1fe
TochuGV <tobiasvallejos05@gmail.com>
2025-02-13 21:23:10 -0300

    Enable CORS and move 'index.js' to src

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import characterRouter from "./routes/character.route.js";
 import movieRouter from "./routes/movie.route.js";
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 app.use('/characters', characterRouter);
 app.use('/movies', movieRouter);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import app from "./src/app.js"
+import app from "./app.js"
 import "dotenv/config";
 
 const port = process.env.PORT || 3000;
