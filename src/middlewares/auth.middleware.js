3188744c5296ae2f5f4471f34b510a87898d64aa
TochuGV <tobiasvallejos05@gmail.com>
2025-02-22 04:36:33 -0300

    Improve error handling in middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,11 +1,12 @@
 import passport from "passport";
 import "dotenv/config"
+import { UnauthorizedError } from "../utils/errors.js";
 
 export const authMiddleware = (req, res, next) => {
     let token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
-    if (!token) return res.status(401).send("Unauthorized");
+    if (!token) throw new UnauthorizedError("Unauthorized");
     passport.authenticate('jwt', {session: false}, (err, user) => {
-        if(err || !user) return res.status(401).send("Unauthorized");
+        if(err || !user) throw new UnauthorizedError("Unauthorized");
         req.user = user
         next();
     })(req, res, next);
--- src/middlewares/validateId.middleware.js
+++ src/middlewares/validateId.middleware.js
@@ -1,7 +1,8 @@
 import { idSchema } from "../schemas/id.schema.js";
+import { BadRequestError } from "../utils/errors.js";
 
 export const validateIdMiddleware = (req, res, next) => {
     const validation = idSchema.safeParse({ id: req.params.id });
-    if (!validation.success) return res.status(400).send("Bad request, invalid ID format.");
+    if (!validation.success) throw new BadRequestError("Invalid ID format.");
     next();
 }
