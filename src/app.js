487e6b27fca313f16b60b6dd2f32d96709a1d166
TochuGV <tobiasvallejos05@gmail.com>
2025-02-26 02:56:49 -0300

    Implement helmet

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import passport from "passport";
+import { limiter } from "./middlewares/rate-limit.middleware.js";
 import { jwtStrategy } from "./config/jwt.strategy.js";
 import authRouter from "./routes/auth.route.js";
 import characterRouter from "./routes/character.route.js";
 import movieRouter from "./routes/movie.route.js";
 import { errorHandler } from "./middlewares/error-handler.middleware.js";
-import { limiter } from "./middlewares/rate-limit.middleware.js";
 
 const app = express();
+app.use(helmet())
 app.use(cors())
 app.use(express.json());
 app.use(cookieParser());
