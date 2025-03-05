fc5a606def40c6e159c65c30a80ea1084b7393d3
TochuGV <tobiasvallejos05@gmail.com>
2025-03-05 19:29:03 -0300

    Move schemas to 'models.json' and update 'index.js'

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -1,10 +1,13 @@
 import fs from "fs";
 
 const swaggerPaths = JSON.parse(fs.readFileSync("./src/swagger/paths.json", "utf-8"));
-const swaggerSchemas = JSON.parse(fs.readFileSync("./src/swagger/schemas.json", "utf-8"));
+const swaggerModels = JSON.parse(fs.readFileSync("./src/swagger/models.json", "utf-8"))
+//const swaggerSchemas = JSON.parse(fs.readFileSync("./src/swagger/schemas.json", "utf-8"));
 const swaggerDocument = JSON.parse(fs.readFileSync("./src/swagger/swagger.json", "utf-8"));
 
 swaggerDocument.paths = swaggerPaths;
-swaggerDocument.components = { schemas: swaggerSchemas };
+swaggerDocument.components = { 
+    schemas: { ...swaggerModels/*, ...swaggerSchemas*/ } 
+};
 
 export default swaggerDocument;
