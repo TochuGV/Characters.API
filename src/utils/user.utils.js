4685889e811d36b3f96b69245e33f4883b80f228
TochuGV <tobiasvallejos05@gmail.com>
2025-03-14 15:37:58 -0300

    Delete dead code

--- src/utils/user.utils.js
+++ src/utils/user.utils.js
@@ -1,10 +1,5 @@
-import { randomUUID } from "crypto";
 import bcrypt from "bcrypt"
 import { CONFIG } from "../common/config.constants.js";
-
-export const generateRandomID = () => {
-    return crypto.randomUUID();
-};
 
 export const getHashedPassword = (password) => {
     return bcrypt.hash(password, CONFIG.SALT_ROUNDS);
