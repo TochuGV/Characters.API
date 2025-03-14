f86be4e571621562b61484a547c52418b66e74b7
TochuGV <tobiasvallejos05@gmail.com>
2025-03-14 17:47:43 -0300

    Merge all Swagger JSON files into 'index.js'

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -1,9 +1,12 @@
 import fs from "fs";
 
 const swagger = JSON.parse(fs.readFileSync("./src/swagger/swagger.json", "utf-8"));
-const models = JSON.parse(fs.readFileSync("./src/swagger/components/models.json", "utf-8"));
-const parameters = JSON.parse(fs.readFileSync("./src/swagger/components/parameters.json", "utf-8"));
 const characterPath = JSON.parse(fs.readFileSync("./src/swagger/paths/characters.json", "utf-8"));
+const schemas = JSON.parse(fs.readFileSync("./src/swagger/components/schemas.json", "utf-8"));
+const responses = JSON.parse(fs.readFileSync("./src/swagger/components/responses.json", "utf-8"));
+const requests = JSON.parse(fs.readFileSync("./src/swagger/components/requests.json", "utf-8"));
+const parameters = JSON.parse(fs.readFileSync("./src/swagger/components/parameters.json", "utf-8"));
+const securitySchemes = JSON.parse(fs.readFileSync("./src/swagger/components/securitySchemes.json", "utf-8"));
 
 swagger.paths = {
 	...swagger.paths,
@@ -13,10 +16,19 @@ swagger.paths = {
 swagger.components = {
 	...swagger.components,
 	schemas: {
-		...models
+		...schemas
+	},
+	responses: {
+		...responses
+	},
+	requests: {
+		...requests
 	},
 	parameters: {
 		...parameters
+	},
+	securitySchemes: {
+		...securitySchemes
 	}
 };
 
