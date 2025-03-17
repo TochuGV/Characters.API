a4c2a90789587276db9f1c45b9bb5c07686470ea
TochuGV <tobiasvallejos05@gmail.com>
2025-03-17 16:34:12 -0300

    Implement 'readJSONFile' function for Swagger

--- src/swagger/index.js
+++ src/swagger/index.js
@@ -1,12 +1,14 @@
 import fs from "fs";
 
-const swagger = JSON.parse(fs.readFileSync("./src/swagger/swagger.json", "utf-8"));
-const characterPath = JSON.parse(fs.readFileSync("./src/swagger/paths/characters.json", "utf-8"));
-const schemas = JSON.parse(fs.readFileSync("./src/swagger/components/schemas.json", "utf-8"));
-const responses = JSON.parse(fs.readFileSync("./src/swagger/components/responses.json", "utf-8"));
-const requests = JSON.parse(fs.readFileSync("./src/swagger/components/requests.json", "utf-8"));
-const parameters = JSON.parse(fs.readFileSync("./src/swagger/components/parameters.json", "utf-8"));
-const securitySchemes = JSON.parse(fs.readFileSync("./src/swagger/components/securitySchemes.json", "utf-8"));
+const readJSONFile = (path) => JSON.parse(fs.readFileSync(path, "utf-8"));
+
+const swagger = readJSONFile("./src/swagger/swagger.json");
+const characterPath = readJSONFile("./src/swagger/paths/characters.json");
+const schemas = readJSONFile("./src/swagger/components/schemas.json");
+const responses = readJSONFile("./src/swagger/components/responses.json");
+const requests = readJSONFile("./src/swagger/components/requests.json");
+const parameters = readJSONFile("./src/swagger/components/parameters.json");
+const securitySchemes = readJSONFile("./src/swagger/components/securitySchemes.json");
 
 swagger.paths = {
 	...swagger.paths,
