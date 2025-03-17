7a237c518cff2eab95a8c09b4491d9b343fa9063
TochuGV <tobiasvallejos05@gmail.com>
2025-03-17 20:53:10 -0300

    Fix 'parseNumericQueryParam' function

--- src/utils/query.utils.js
+++ src/utils/query.utils.js
@@ -1,9 +1,7 @@
-import z from "zod";
-
 export const parseNumericQueryParam = (val) => {
-    if (typeof val === "string") {
+    if (typeof val === "string" && val.trim() !== "") {
         const parsed = Number(val);
-        return isNaN(parsed) ? undefined : parsed;
-    }
+        if(!isNaN(parsed)) return parsed;
+    };
     return val;
 };
