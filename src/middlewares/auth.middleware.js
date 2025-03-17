8fabee8f08a6c7a92d75b251210915455d3bb3f1
TochuGV <tobiasvallejos05@gmail.com>
2025-03-17 20:59:18 -0300

    Remove unnecessary import

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,13 +1,12 @@
 import passport from "passport";
-import "dotenv/config"
 import { ErrorFactory } from "../common/errors/error-factory.js";
 
 export const authMiddleware = (req, res, next) => {
-    let token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
-    if (!token) throw ErrorFactory.createError("UNAUTHORIZED");
-    passport.authenticate('jwt', {session: false}, (err, user) => {
-        if(err || !user) throw ErrorFactory.createError("UNAUTHORIZED");
-        req.user = user
-        next();
-    })(req, res, next);
+	let token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
+	if (!token) throw ErrorFactory.createError("UNAUTHORIZED");
+	passport.authenticate('jwt', {session: false}, (err, user) => {
+		if(err || !user) throw ErrorFactory.createError("UNAUTHORIZED");
+		req.user = user
+		next();
+	})(req, res, next);
 };
