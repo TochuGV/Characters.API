9e6b8b1e1522e62629d2cc9b2781f57e2684dc22
TochuGV <tobiasvallejos05@gmail.com>
2025-03-16 23:38:03 -0300

    Move 'secret' variable to global scope in 'token.utils.js'

--- src/utils/token.utils.js
+++ src/utils/token.utils.js
@@ -1,12 +1,13 @@
 import jwt from "jsonwebtoken";
 import { CONFIG } from "../common/config.constants.js";
 
+const secret = CONFIG.JWT_SECRET_KEY;
+
 export const generateToken = (user) => {
-	const secret = CONFIG.JWT_SECRET_KEY;
 	const expiresIn = CONFIG.JWT_EXPIRES_IN;
 	return jwt.sign({ id: user.ID, email: user.Email }, secret, { expiresIn });
 };
 
 export const isValidToken = (token) => {
-	return Boolean(jwt.verify(token, CONFIG.JWT_SECRET_KEY, (err) => !err));
+	return Boolean(jwt.verify(token, secret, (err) => !err));
 };
