321505ecc68977e774da073ad7e208da8d0aaf58
TochuGV <tobiasvallejos05@gmail.com>
2025-12-01 18:28:16 -0300

    Fix code formatting and whitespace consistency

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -5,7 +5,7 @@ export const authMiddleware = (req, res, next) => {
   let token = req.cookies.jwt || req.headers.authorization?.split(" ")[1];
   if (!token) throw errorFactory.createError("UNAUTHORIZED");
   passport.authenticate('jwt', {session: false}, (err, user) => {
-    if(err || !user) throw errorFactory.createError("UNAUTHORIZED");
+    if (err || !user) throw errorFactory.createError("UNAUTHORIZED");
     req.user = user
     next();
   })(req, res, next);
--- src/middlewares/error-handler.middleware.js
+++ src/middlewares/error-handler.middleware.js
@@ -1,7 +1,7 @@
 export const errorHandler = (err, req, res, next) => {
   console.error(err);
   let statusCode = err.statusCode || 500;
-  if(err.message.includes("Database") || err.message.includes("Failed to connect to the database")) statusCode = 503;
+  if (err.message.includes("Database") || err.message.includes("Failed to connect to the database")) statusCode = 503;
   const response = { error: err.isOperational ? err.message : "Something went wrong!" };
   if (err.details && err.isOperational) response.details = err.details;
   return res.status(statusCode).json(response);
