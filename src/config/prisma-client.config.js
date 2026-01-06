584ba89ac4075029dad8496ad374e4c96a8f98fd
TochuGV <tobiasvallejos05@gmail.com>
2026-01-06 02:41:44 -0300

    Fix Prisma ORM setup for SQL Server

--- src/config/prisma-client.config.js
+++ src/config/prisma-client.config.js
@@ -1,12 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaMssql } from "@prisma/adapter-mssql";
 import env from "./enviroment.config.js";
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: env.DB_URL
-    }
-  }
-});
+const adapter = new PrismaMssql(env.DB_URL);
+const prisma = new PrismaClient({ adapter })
 
 export default prisma;
