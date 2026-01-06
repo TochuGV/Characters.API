00fbc4a21ea6c38a1c0c6073f1565993a3ba60e7
TochuGV <tobiasvallejos05@gmail.com>
2026-01-06 16:37:56 -0300

    Migrate UserRepository to Prisma

--- src/repositories/user.repository.js
+++ src/repositories/user.repository.js
@@ -1,21 +1,16 @@
-import { getConnection, sql } from "../database/connection.js";
+import prisma from "../config/prisma-client.config.js"
 
 export default class UserRepository {
   async getByEmail(email){
-    const pool = await getConnection();
-    const result = await pool.request()
-      .input('pEmail', sql.VarChar, email)
-      .execute('GetUserByEmail');
-    return result.recordset[0];
+    return await prisma.user.findFirst({
+      where: { email }
+    });
   };
 
   async create(email, password){
-    const pool = await getConnection();
-    const hashedPassword = await getHashedPassword(password);
-    const result = await pool.request()
-      .input('pEmail', sql.VarChar, email)
-      .input('pPassword', sql.VarChar, hashedPassword)
-      .execute('CreateUser');
-    return result.recordset?.[0]?.RowsAffected > 0;
+    const user = await prisma.user.create({
+      data: { email, password }
+    });
+    return !!user;
   };
 };
