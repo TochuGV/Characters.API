e9e80b85c53594cdf97629aed13de2c48adc430e
TochuGV <tobiasvallejos05@gmail.com>
2026-01-07 23:56:12 -0300

    Optimize AppError constructor and inheritance logic

--- src/errors/app-error.js
+++ src/errors/app-error.js
@@ -1,7 +1,8 @@
-export default class AppError extends Error{
-  constructor(message, statusCode, isOperational = true){
+export default class AppError extends Error {
+  constructor(message, statusCode, details = null, isOperational = true) {
     super(message);
     this.statusCode = statusCode;
+    if (details) this.details = details;
     this.isOperational = isOperational;
     Error.captureStackTrace(this, this.constructor);
   };
